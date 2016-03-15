045448f3ac5a6cd8f4add798098f164fd81a36d1
Sameen Islam <ec14370@qmul.ac.uk>
2016-03-15 22:32:28 +0000

    Precipitation screen now shows time and we can now return to the main menu.

---
+++ src/PrecipitationController.java
@@ -0,0 +1,66 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+/**
+ * Created by sameenislam on 15/03/2016.
+ */
+public class PrecipitationController implements Initializable{
+
+    @FXML
+    public Label lblCurrentTime;
+    @FXML
+    public AnchorPane crwPane = new AnchorPane();
+
+    // displays the current time in 24hr format (HH:mm)
+    public void showTime() {
+        //final DateFormat format = DateFormat.getInstance();
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                final Calendar cal = Calendar.getInstance();
+
+                lblCurrentTime.setText(simpleDateFormat.format(cal.getTime()));
+            }
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        showTime();
+
+        // detects whether mouse is being right-clicked
+        // used for invoking the main menu
+        crwPane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if (t.isSecondaryButtonDown()) {
+                    System.out.println("secondary engaged");
+                    // invoke main menu
+                    try {
+                        Main.showMainMenu();
+                    } catch (IOException e) {
+                        System.out.println("An I/O exception occurred!");
+                    }
+                }
+            }
+        });
+    }
+}
