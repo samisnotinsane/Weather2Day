0dc8fd2931968e230b5348ddbbc5d8cbe1cdac8b
Sameen Islam <ec14370@qmul.ac.uk>
2016-03-15 22:20:56 +0000

    Added functionality to go back to main menu from temperature screen.

--- src/TemperatureController.java
+++ src/TemperatureController.java
@@ -1,15 +1,20 @@
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
+
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
-import java.text.DateFormat;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.ResourceBundle;
@@ -20,7 +25,10 @@ import java.util.ResourceBundle;
 public class TemperatureController implements Initializable {
     @FXML
     public Label lblCurrentTime;
+    @FXML
+    public AnchorPane tempPan = new AnchorPane();
 
+    // displays the current time in 24hr format (HH:mm)
     public void showTime() {
         //final DateFormat format = DateFormat.getInstance();
         final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
@@ -36,9 +44,45 @@ public class TemperatureController implements Initializable {
         timeline.play();
     }
 
+    // detects whether mouse is being clicked and being kept held
+    // used for invoking the main menu
+    // code source: http://stackoverflow.com/questions/25601266/how-to-achieve-javafx-mouse-event-push-and-hold
+//    private void addPressAndHoldHandler(Node node, Duration holdTime, EventHandler<MouseEvent> handler) {
+//        class Wrapper<T> { T content ; }
+//        Wrapper<MouseEvent> eventWrapper = new Wrapper<>();
+//
+//        PauseTransition holdTimer = new PauseTransition(holdTime);
+//        holdTimer.setOnFinished(event -> handler.handle(eventWrapper.content));
+//
+//
+//        node.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+//            eventWrapper.content = event ;
+//            holdTimer.playFromStart();
+//        });
+//        node.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> holdTimer.stop());
+//        node.addEventHandler(MouseEvent.DRAG_DETECTED, event -> holdTimer.stop());
+//    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         showTime();
+
+        // detects whether mouse is being right-clicked
+        // used for invoking the main menu
+        tempPan.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if (t.isSecondaryButtonDown()) {
+                    System.out.println("secondary engaged");
+                    // invoke main menu
+                    try {
+                        Main.showMainMenu();
+                    } catch (IOException e) {
+                        System.out.println("An I/O exception occurred!");
+                    }
+                }
+            }
+        });
 
 
     }
